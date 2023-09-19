631e2731aadc8169668e4cc0ee38436c7c2f4132
JonatanLinnKTH <jlinn@kth.se>
2023-09-19 22:37:48 +0200

    Inleder föreläsningen

---
+++ föreläsningar/föreläsnig moduler och paket/huvudprogram.py
@@ -0,0 +1,15 @@
+"""
+Föreläsning rekursion, for, while 18/9
+Skriven av Jonatan Linn
+Kurskod: DD1310
+"""
+import test_modul
+import test_modul_2
+
+# testar modulkonseptet
+# test_modul.hello_world()
+
+# testar __name__ variabeln
+# print(__name__)
+
+test_modul_2.matar_du_in_ett_heltal()
---
+++ föreläsningar/föreläsnig moduler och paket/test_modul.py
@@ -0,0 +1,10 @@
+def hello_world():
+    """SKriver ut Hello World!!!"""
+    print("Hello World!!!")
+    print(__name__)
+
+if __name__ == "__main__":
+    hello_world()
+
+print(__name__)
+    
